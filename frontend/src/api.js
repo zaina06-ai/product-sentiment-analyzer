7545416fcd805917d0ffd4a176ff5ccb4d101a5f
ZAINA RAHEEN A P <raheenzaina06@gmail.com>
2026-07-06 16:34:07 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = "https://product-sentiment-analyser.onrender.com";
 
 /**
  * Searches for a product and returns its sentiment analysis.
